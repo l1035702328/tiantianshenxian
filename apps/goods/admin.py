92123fa9df798da1dac0eafc02e649d5c4850ee0
v_jjinnlli <1156989490@qq.com>
2022-06-29 10:45:17 +0800

    初始化

---
+++ apps/goods/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.core.cache import cache
+from apps.goods.models import GoodsType, IndexPromotionBanner, IndexGoodsBanner, IndexTypeGoodsBanner, Goods, GoodsSKU
+# Register your models here.
+
+from django.contrib import admin
+
+# Register your models here.
+
+
