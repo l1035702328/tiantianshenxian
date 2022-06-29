dbc0b52957df8119aca04314e969b3f564b1ed31
v_jjinnlli <1156989490@qq.com>
2022-06-29 10:43:51 +0800

    初始化

---
+++ apps/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/6/24 16:52
+# @Author  : LZZ
+# @FileName: __init__.py.py
+# @Software: PyCharm
---
+++ db/base_model.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/6/24 16:46
+# @Author  : LZZ
+# @FileName: base_model.py
+# @Software: PyCharm
+from django.db import models
+
+class BaseModel(models.Model):
+    """模型抽象基类
+        使所有模型类继承这些字段"""
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    update_time = models.DateTimeField(auto_now=True, verbose_name='更新时间')
+    is_delete = models.BooleanField(default=False, verbose_name='删除标记')
+
+    class Meta:
+        # 说明是一个抽象模型类
+        abstract = True
